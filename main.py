401f734409886350e221ec8495e47b4fc0d0c551
Luna <102811601+Luna5829@users.noreply.github.com>
2023-11-24 23:25:37 +0100

    Made it case insensitive, changed logs, reformatted some stuff
    Made it case insensitive by converting everything to lowercase
    
    Added colored logs because pure "logging" is only red
    
    Changed LOG_FORMAT to look prettier 😋
    Added some newlines to stuff because most functions were just big blobs of text which made it pretty unreadable ;-;
    Changed double quotes to single quotes (cuz it looks better)
    Changed "Input offset" to DEBUG and made it print the current tas line

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from pathlib import Path
-import time
 import logging
+import time
+from pathlib import Path
 from typing import Iterator
 
+from colorlog import ColoredFormatter
 from pynput import keyboard
 from win32gui import GetWindowText, GetForegroundWindow
 
 from config import INPUT_FILEPATH, INPUT_MAP, FPS
 
+INPUT_MAP = {i.lower(): INPUT_MAP[i] for i in list(INPUT_MAP)}  # Converts keys to lowercase for case insensitivity
+
 KeyType = str | keyboard.Key
 InputKeyframeType = tuple[int, set[KeyType]]
 
-LOG_FORMAT = "[%(asctime)s %(levelname)s] %(message)s"
-logging.basicConfig(format=LOG_FORMAT)
-logging.info("Logger initialized")
+#  Initializes colored logs (pure logging is only red ;-;)
+LOG_FORMAT = '  %(log_color)s%(levelname)-7s%(reset)s | %(log_color)s%(message)s%(reset)s'
+logging.root.setLevel(logging.DEBUG)
+stream = logging.StreamHandler()
+stream.setFormatter(ColoredFormatter(LOG_FORMAT))
+logging.root.addHandler(stream)
 
 
 def try_int(num: str, fail_message: str) -> int:
     try:
         return int(num)
+
     except ValueError:
         logging.error(fail_message)
         raise
@@ -26,14 +33,17 @@ def try_int(num: str, fail_message: str) -> int:
 
 def parse_inputs(input_names: list[str], lineno: int) -> set[KeyType]:
     result = set()
+
     for name in input_names:
         if not name:
             continue
+
         key = INPUT_MAP.get(name)
         if key is None:
             logging.error(f"Invalid input name on line {lineno}: {name}")
             raise ValueError(f"Invalid input name: {name}")
         result.add(key)
+
     return result
 
 
@@ -41,22 +51,28 @@ def parse_lines(lines: Iterator[str]) -> list[InputKeyframeType]:
     result = []
     repeating_count: int = 0  # 0 for not repeating, [repeat count] for repeating
     current_repeat: list[InputKeyframeType] = []
+
     for lineno, line in enumerate(lines, 1):
         parts = [part.strip() for part in line.split(",")]
+
         match parts:
             case ():  # empty line
                 continue
+
             case first, *_ if first.startswith("#"):
                 continue
+
             case "REPEAT", count_str:
                 if repeating_count:
                     logging.error("Nested repeats are not supported")
                     raise ValueError("Nested repeats are not supported")
                 repeating_count = try_int(count_str, "Repeat count must be an integer")
                 current_repeat = []
+
             case "ENDREPEAT", :
                 result += current_repeat * repeating_count
                 repeating_count = 0
+
             case duration_str, *input_names:
                 duration = try_int(duration_str, "Duration must be an integer")
                 inputs = parse_inputs(input_names, lineno)
@@ -64,9 +80,11 @@ def parse_lines(lines: Iterator[str]) -> list[InputKeyframeType]:
                     result.append((duration, inputs))
                 else:
                     current_repeat.append((duration, inputs))
+
     if repeating_count:
         logging.error("Unfinished REPEAT")
         raise ValueError("Unfinished REPEAT")
+
     return result
 
 
@@ -80,6 +98,7 @@ def read_inputs(fp: str | Path) -> list[InputKeyframeType]:
     except (OSError, IOError) as e:
         logging.error(f"Error while reading input file: {e}")
         raise
+
     return inputs
 
 
@@ -90,12 +109,12 @@ def execute_inputs(inputs: list[InputKeyframeType]) -> None:
     total_duration = 0
 
     while GetWindowText(GetForegroundWindow()) != 'Strata':
-        time.sleep(0.1)  # Waiting for you to switch to Strata
+        ...  # Waiting for you to switch to Strata
     time.sleep(0.2)
 
     start_time = time.time()
     controller = keyboard.Controller()
-    for duration, keys in inputs:
+    for line, (duration, keys) in enumerate(inputs):
         if GetWindowText(GetForegroundWindow()) != 'Strata':
             logging.warning('Tabbed out of Strata, ending execution')
             break
@@ -103,18 +122,23 @@ def execute_inputs(inputs: list[InputKeyframeType]) -> None:
         # Release old keys
         for key in current_keys - keys:
             controller.release(key)
+
         # Press new keys
         for key in keys - current_keys:
             controller.press(key)
+
         # Display offset
         current_time = total_duration / FPS
         actual_time = time.time() - start_time
-        logging.info(f'Input offset: {current_time - actual_time:.4f}s')
+        logging.info(f'Input offset: {current_time - actual_time:.4f}s (line {line + 1})')
+
         # Wait until current keyframe ends
         total_duration += duration
         end_time = start_time + total_duration / FPS
+
         while time.time() < end_time:
             time.sleep(0.001)
+
         current_keys = keys
 
     for key in INPUT_MAP.values():
